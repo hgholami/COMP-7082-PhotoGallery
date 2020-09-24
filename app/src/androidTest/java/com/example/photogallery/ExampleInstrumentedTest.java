9a36a7c7985904e0816682bfa4b1d6b0c3fbc0af
zzhang129@my.bcit.ca <zzhang129@my.bcit.ca>
2020-09-24 14:59:46 -0700

    Adjusted UITest

--- app/src/androidTest/java/com/example/photogallery/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/photogallery/ExampleInstrumentedTest.java
@@ -1,29 +1,44 @@
 package com.example.photogallery;
 
-        import androidx.test.ext.junit.runners.AndroidJUnit4;
-        import androidx.test.filters.LargeTest;
-        import androidx.test.rule.ActivityTestRule;
+import android.content.Context;
 
-        import org.junit.Rule;
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
-        import static androidx.test.espresso.Espresso.onView;
-        import static androidx.test.espresso.action.ViewActions.click;
-        import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-        import static androidx.test.espresso.action.ViewActions.typeText;
-        import static androidx.test.espresso.assertion.ViewAssertions.matches;
-        import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-        import static androidx.test.espresso.matcher.ViewMatchers.withId;
-        import static androidx.test.espresso.matcher.ViewMatchers.withText;
-        import static org.hamcrest.core.StringContains.containsString;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class ExampleInstrumentedTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.photogallery", appContext.getPackageName());
+    }
 
     @Test
     public void searchCaptionTest() {
@@ -115,5 +130,4 @@ public class ExampleInstrumentedTest {
         onView(withId(R.id.leftButton)).perform(click());
         onView(withId(R.id.editCaption)).check(matches(withText(containsString("4"))));
     }
-
 }
