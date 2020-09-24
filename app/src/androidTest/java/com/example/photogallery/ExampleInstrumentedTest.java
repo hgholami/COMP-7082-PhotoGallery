d315789a4ff3c6003481651fda00959eac67491f
zzhang129@my.bcit.ca <zzhang129@my.bcit.ca>
2020-09-24 14:52:17 -0700

    Changed UITest and move it to ExampleInsrumentedTest.java

--- app/src/androidTest/java/com/example/photogallery/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/photogallery/ExampleInstrumentedTest.java
@@ -1,26 +1,119 @@
 package com.example.photogallery;
 
-import android.content.Context;
+        import androidx.test.ext.junit.runners.AndroidJUnit4;
+        import androidx.test.filters.LargeTest;
+        import androidx.test.rule.ActivityTestRule;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+        import org.junit.Rule;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+        import static androidx.test.espresso.Espresso.onView;
+        import static androidx.test.espresso.action.ViewActions.click;
+        import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+        import static androidx.test.espresso.action.ViewActions.typeText;
+        import static androidx.test.espresso.assertion.ViewAssertions.matches;
+        import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+        import static androidx.test.espresso.matcher.ViewMatchers.withId;
+        import static androidx.test.espresso.matcher.ViewMatchers.withText;
+        import static org.hamcrest.core.StringContains.containsString;
 
-import static org.junit.Assert.*;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.photogallery", appContext.getPackageName());
+    public void searchCaptionTest() {
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.keywordField)).check(matches(isDisplayed()));
+        onView(withId(R.id.keywordField)).perform(typeText("2"), closeSoftKeyboard());
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));//should be 2
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.keywordField)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.rightButton)).perform(click());//1
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("1"))));
+        onView(withId(R.id.rightButton)).perform(click());//2
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.rightButton)).perform(click());//3
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("3"))));
+        onView(withId(R.id.leftButton)).perform(click());//2
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.leftButton)).perform(click());//1
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("1"))));
+        onView(withId(R.id.leftButton)).perform(click());//4
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("4"))));
     }
+    @Test
+    public void searchDateTest() {
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.keywordField)).check(matches(isDisplayed()));
+        onView(withId(R.id.startDateField)).perform(typeText("2019-10-10"), closeSoftKeyboard());
+        onView(withId(R.id.endDateField)).perform(typeText("2025-10-10"), closeSoftKeyboard());
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("4"))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("1"))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("3"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("1"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("4"))));
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.startDateField)).perform(typeText("2019-10-10"), closeSoftKeyboard());
+        onView(withId(R.id.endDateField)).perform(typeText("2019-10-11"), closeSoftKeyboard());
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString(""))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString(""))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString(""))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString(""))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString(""))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString(""))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString(""))));
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.startDateField)).perform(typeText("2019-10-10"), closeSoftKeyboard());
+        onView(withId(R.id.endDateField)).perform(typeText("2025-10-10"), closeSoftKeyboard());
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("4"))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("1"))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.rightButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("3"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("2"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("1"))));
+        onView(withId(R.id.leftButton)).perform(click());
+        onView(withId(R.id.editCaption)).check(matches(withText(containsString("4"))));
+    }
+
 }
