2016ba51ba57eb8d35972edc5fb3c0b3be5fb4aa
hgholami <35938542+hgholami@users.noreply.github.com>
2020-10-22 21:17:24 -0700

    Implemented Sudo AOP Code

--- app/src/main/java/com/example/photogallery/AspectLogging.java
+++ app/src/main/java/com/example/photogallery/AspectLogging.java
@@ -2,16 +2,26 @@ package com.example.photogallery;
 
 import android.util.Log;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+
+import java.io.File;
 
 @Aspect
 public class AspectLogging {
 
-    @Before("   call(void navGallery(..))")
-    public void beforeNavButtonPress() {
-        Log.d("LOGGING", "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    //After createImageFile() is called, log the name and directory of file added to images
+    @After("execution(File *.createImageFile(..)) && args(filePath)")
+    public void afterCreateImageFile(File filePath) {
+        Log.d("FILE CREATED", filePath.toString());
+    }
+
+    //@AfterReturning("execution(* *.getPhoto(..))")
+    @AfterReturning(pointcut="execution(* *.getPhoto(..))", returning="returnValue")
+    public void logGetPhotoReturn(String returnValue) {
+        Log.d("GOT PHOTO", returnValue);
     }
 }
